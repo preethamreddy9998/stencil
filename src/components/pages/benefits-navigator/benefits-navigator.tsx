eec15298f196867141714d534a9a5e571f898401
Boddu, Preetham <Preetham.Boddu@nexteraenergy.com>
2021-04-15 13:03:06 -0400

    header Footer

--- src/components/pages/benefits-navigator/benefits-navigator.tsx
+++ src/components/pages/benefits-navigator/benefits-navigator.tsx
@@ -11,64 +11,101 @@ export class BenefitsNavigator {
   render() {
     return (
     <div class="main_container row">
-    <div class="top_banner">
-      <img class="banner_image" src="assets/img/5db73cd5dba56c10c8f29c46b996192b.iix"/>
-      <div style={{textAlign: 'center'}}>
-        <input type="text" class="search_ele" placeholder="Search.."/>
-         <p>Benefits Navigator</p>
+    <div class="top_banner benefits_banner col-md-12">
+        <h1>Benefits Navigator</h1>
          <p2>Know where to go when you need care.  Explore our interactive guide below to find your best options for care.</p2>
+    </div>
+    <form class="col-md-12 top_banner_form">
+      <div class="form-row">
+      <div class="col-md-4 mb-3"></div>
+      <div class="col-md-4 mb-3">
+        <div class="input-group">
+         
+          <input type="text" class="form-control form-control-lg" placeholder="Search.."  />
+          <div class="input-group-append input-group-append-lg">
+            <span class="input-group-text input-group-text-lg">@</span>
+          </div>
+          
+          
+        </div>
+      </div>
+      </div>
+    </form>
+    
+    <div class="container">
+      <div class="row">
+          <div class="col-lg-6 col-md-12 col-sm-12">
+          <div class="card" >
+            <div class="card-body">
+              <img class="card_image" src="assets/img/kwtg_healthhub.png"/>
+              <p>I need preventive care</p>
+            </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-12 col-sm-12">
+          <div class="card" >
+    <div class="card-body">
+      <img class="card_image" src="assets/img/Get_The_Right_Care.png"/>
+      <p>I'm sick or injured</p>
+    </div>
+    </div>
+          </div>
+          <div class="col-lg-6 col-md-12 col-sm-12">
+          <div class="card" >
+    <div class="card-body">
+      <img class="card_image" src="assets/img/chart.png"/>
+      <p>I have an on-going condition or serious diagnosis</p>
+    </div>
+    </div>
+          </div>
+
+          <div class="col-lg-6 col-md-12 col-sm-12">
+
+          <div class="card" >
+    <div class="card-body">
+      <img class="card_image" src="assets/img/rx.png"/>
+      <p>I need to fill prescription</p>
+    </div>
+    </div>
+    </div>
+          <div class="col-lg-6 col-md-12 col-sm-12">
+    <div class="card" >
+    <div class="card-body">
+      <img class="card_image" src="assets/img/mhh.png"/>
+      <p>Find a provider, facility or procedure</p>
+    </div>
+    </div>
+    </div>
+          <div class="col-lg-6 col-md-12 col-sm-12">
+    <div class="card" >
+    <div class="card-body">
+      <img class="card_image" src="assets/img/provider.png"/>
+      <p>Preferred facilities</p>
+    </div>
+    </div>
+    </div>
+          <div class="col-lg-6 col-md-12 col-sm-12">
+    <div class="card" >
+    <div class="card-body">
+      <img class="card_image" src="assets/img/bcbs.png"/>
+      <p>Blue Cross Blue Shield customer service</p>
+    </div>
+    </div>
+    </div>
+          <div class="col-lg-6 col-md-12 col-sm-12">
+    <div class="card" >
+    <div class="card-body">
+      <img class="card_image" src="assets/img/eap2.png"/>
+      <p>Emotional, psychological, and social well-being</p>
+    </div>
+    </div>
+    </div>
+
+          
+
+
       </div>
       
-    </div>
-    <div class="container card_container">
-      <div class="card" >
-        <div class="card-body">
-          <img class="card_image" src="assets/img/kwtg_healthhub.png"/>
-          <p>I need preventive care</p>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <img class="card_image" src="assets/img/Get_The_Right_Care.png"/>
-          <p>I'm sick or injured</p>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <img class="card_image" src="assets/img/chart.png"/>
-          <p>I have an on-going condition or serious diagnosis</p>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <img class="card_image" src="assets/img/rx.png"/>
-          <p>I need to fill prescription</p>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <img class="card_image" src="assets/img/mhh.png"/>
-          <p>Find a provider, facility or procedure</p>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <img class="card_image" src="assets/img/provider.png"/>
-          <p>Preferred facilities</p>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <img class="card_image" src="assets/img/bcbs.png"/>
-          <p>Blue Cross Blue Shield customer service</p>
-        </div>
-        </div>
-        <div class="card" >
-        <div class="card-body">
-          <img class="card_image" src="assets/img/eap2.png"/>
-          <p>Emotional, psychological, and social well-being</p>
-        </div>
-        </div>
       </div>
   </div>)
   }
--- src/components/shared/footer-component/footer-component.tsx
+++ src/components/shared/footer-component/footer-component.tsx
@@ -8,33 +8,85 @@ export class FooterComponent {
 
   render() {
     return (
-      <div class="footer">
-				<div class="banner_image">
-					<img class="ban_image" src="assets/img/5db73cd5dba56c10c8f29c46b996192b.iix"/>
-				</div>
-				<p>
-                <b>Quick links</b>
-			    </p>
-
-				<div class="container quick_links">
-					<div class="quick_link_details">
-						<img src="assets/img/link.jpg" class="link_image" />
-						Hr polices
-					</div>
-					<div class="quick_link_details">
-						<img src="assets/img/link.jpg" class="link_image" />
-						HR4U
-					</div>
-					<div class="quick_link_details">
-						<img src="assets/img/link.jpg" class="link_image" />
-						Org charts
-					</div>
-					<div class="quick_link_details">
-						<img src="assets/img/link.jpg" class="link_image" />
-						myHealthHub - BCBS
-					</div>
-				</div>
+		<footer class="row">
+			<div class="col-md-12 column kang"> 
+				<div class="panel-heading"> 
+					<h2>Not finding what you're looking for?<b> We're here to help!</b></h2> 
+				</div> 
+			</div> 
+			<div class="col-md-12 column kang"> 
+				<div class="row"> 
+					<div class="col-md-3 column ui-sortable"></div> 
+					<div class="col-md-3 column ui-sortable"> 
+							<div><div class="post-container"> 
+							<div class="post-thumb"> 
+							<img src="assets/img/hrsubmitRequest.png"  role="presentation"/>
+						</div> 
+						<div class="post-content"> 
+						<h3 class="post-title">Ask HR4U</h3> 
+						<p>Ask a question or submit a request to HR.</p> 
+						<a class="btn" href="/esc?id=esc_sc_category"> SUBMIT A REQUEST </a> 
+						</div> 
+						</div></div> 
+					</div> 
+					<div class="col-md-3 column ui-sortable"> 
+						<div><div class="post-container"> 
+						<div class="post-thumb">
+						<img src="assets/img/hrcontactUsfooter.png" role="presentation"/>
+						</div> 
+						<div class="post-content"> 
+						<h3 class="post-title"> Contact us </h3> 
+						<p>Still need some additional support? Employee Services is here to help. Contact us at 844-694-HR4U (4748). </p>
+						</div> 
+						</div></div> 
+					</div> 
+					<div class="col-md-3 column ui-sortable"></div> 
+				</div> 
 			</div>
+			<div class="mainquicklinks col-md-12">
+				<div class="row justify-content-md-center">
+					<div class="col-md-3"></div>
+					<div class="col-md-9">
+						<p><b>QUICK LINKS</b></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3"></div>
+					<div class="col-md-6">
+						<div class="row">
+                   
+				   <div class="col-md-4 btmmainquicklinks">
+					   <img ng-src="quick link.svg" height="32" width="32" role="presentation" src="assets/img/quicklink.svg"/>
+					   <a ng-href="http://sapredirect.fpl.com/eweb.html?url=global/policies//indexCorp.shtml" target="_blank" href="http://sapredirect.fpl.com/eweb.html?url=global/policies//indexCorp.shtml"><b class="ng-binding">HR policies</b></a>
+				   </div>
+				   
+				   <div class="col-md-4 btmmainquicklinks">
+					   <img ng-src="quick link.svg" height="32" width="32" role="presentation" src="assets/img/quicklink.svg"/>
+					   <a ng-href="https://performancemanager4.successfactors.com/sf/start?company=NEE" target="_blank" href="https://performancemanager4.successfactors.com/sf/start?company=NEE"><b class="ng-binding">HR4U</b></a>
+				   </div>
+				   
+				   <div class="col-md-4 btmmainquicklinks">
+					   <img ng-src="quick link.svg" height="32" width="32" role="presentation" src="assets/img/quicklink.svg"/>
+					   <a ng-href="https://orgmanagerd48309b38-d7d6b3da3.us2.hana.ondemand.com/orgmanager/orghtml/index.html" target="_blank" href="https://orgmanagerd48309b38-d7d6b3da3.us2.hana.ondemand.com/orgmanager/orghtml/index.html"><b class="ng-binding">Org charts</b></a>
+				   </div>
+				   
+				   <div class="col-md-4 btmmainquicklinks">
+					   <img ng-src="quicklink.svg" height="32" width="32" role="presentation" src="assets/img/quicklink.svg"/>
+					   <a ng-href="http://nexteraenergy.com/myHealthHub" target="_blank" href="http://nexteraenergy.com/myHealthHub"><b class="ng-binding">myHealthHub - BCBS</b></a>
+				   </div>
+				   
+			   </div>
+					</div>
+					<div class="col-md-3"></div>
+				</div>
+        
+        <div class="col-md-12 column ui-sortable">
+            <div class="center">
+                <img src="assets/img/hrfooter-logo.png" width="71" height="31" role="presentation"/> All rights reserved, NextEra Energy Inc. 2020
+            </div>
+        </div>
+    </div>
+		</footer>
     );
   }
 
